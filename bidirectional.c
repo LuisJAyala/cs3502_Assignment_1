b02fbc8f2f08a88b1a6d50af2c452a001ca9ee59
Luis Ayala <luisjulayala@gmail.com>
2026-06-19 23:28:13 -0400

    Updated Bi and Pipe

--- bidirectional.c
+++ bidirectional.c
@@ -3,54 +3,114 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <signal.h>
+
+
+
+
+
+volatile sig_atomic_t shutdown_flag = 0;
+void handle_signals(int sig) {
+    if (sig == SIGINT) {
+        shutdown_flag = 1;
+        printf("\n[Signal] SIGINT caught. Setting shutdown flag to 1...\n");
+    } else if (sig == SIGUSR1) {
+
+        printf("\n[Signal] SIGUSR1 caught. [Stats]: Pipeline running smoothly.\n");
+
+    }
+
 
 int main() {
+
 
     int pipe1[2]; 
     int pipe2[2]; 
     pid_t pid;
     char buffer[100];
-	if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
-        perror("Pipe creation unsuccesful");
+    struct sigaction sa;
+
+    sa.sa_handler = handle_signals;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGUSR1, &sa, NULL);
+
+
+    if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
+        perror("Pipe creation unsuccessful");
         exit(EXIT_FAILURE);
     }
 
     pid = fork();
     if (pid < 0) {
-        perror("Fork unsuccesful");
+        perror("Fork unsuccessful");
         exit(EXIT_FAILURE);
     }
+
     if (pid == 0) {
-        close(pipe1[1]);
-        close(pipe2[0]);
-       memset(buffer, 0, sizeof(buffer));
-        read(pipe1[0], buffer, sizeof(buffer) - 1);
-        printf("Child received: %s\n", buffer);
-        char *child_response = "Hi from child!";
-        write(pipe2[1], child_response, strlen(child_response) + 1);
+
+        close(pipe1[1]); 
+        close(pipe2[0]); 
+
+
+        while (!shutdown_flag) {
+            memset(buffer, 0, sizeof(buffer));
+
+            int bytes_read = read(pipe1[0], buffer, sizeof(buffer) - 1);
+            if (bytes_read <= 0) {
+
+                break; 
+            }
+
+            printf("Child received: %s\n", buffer);
+
+
+            char *child_response = "Hi from child!";
+            if (write(pipe2[1], child_response, strlen(child_response) + 1) == -1) {
+                perror("Child write failed");
+                break;
+            }
+        }
+
 
         close(pipe1[0]);
         close(pipe2[1]);
-     exit(EXIT_SUCCESS);
+        printf("[Child] Exiting gracefully.\n");
+        exit(EXIT_SUCCESS);
+
     } else {
 
-        close(pipe1[0]);
-        close(pipe2[1]);
- 
-      char *parent_message = "Hi from parent!";
-        printf("Parent sending: %s\n", parent_message);
-        write(pipe1[1], parent_message, strlen(parent_message) + 1);
+        close(pipe1[0]); 
+        close(pipe2[1]); 
 
+        char *parent_message = "Hi from parent!";
         
-        memset(buffer, 0, sizeof(buffer));
-        read(pipe2[0], buffer, sizeof(buffer) - 1);
-        printf("Parent received back: %s\n", buffer);// Clean up remaining open ends
-        close(pipe1[1]);
+
+        while (!shutdown_flag) {
+            printf("Parent sending: %s\n", parent_message);
+            
+            if (write(pipe1[1], parent_message, strlen(parent_message) + 1) == -1) {
+                perror("Parent write failed");
+                break;
+            }
+
+            memset(buffer, 0, sizeof(buffer));
+
+            if (read(pipe2[0], buffer, sizeof(buffer) - 1) > 0) {
+                printf("Parent received back: %s\n", buffer);
+            }
+
+            sleep(2); 
+        }
+
+       close(pipe1[1]);
         close(pipe2[0]);
 
-        
         wait(NULL);
+        printf("[Parent] Exiting gracefully.\n");
     }
+
     return 0;
 }
-
--- simple_pipe.c
+++ simple_pipe.c
@@ -1,54 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 
+
+
 int main() {
-	int pipefd[2];
-	pid_t pid;
-	char buffer[100];
-	char *message = "hello from parent!";
 
-	if (pipe(pipefd) == -1) {
-	perror("pipe failed"); //found perror on internet. Thank you Google!
-	return 1;
+    int pipefd[2];
+
+    pid_t pid;
+
+    char buffer[100];
+
+    char *message = "hello from parent!";
 
 
-	pid = fork();
-	if (pid == -1) {
-		perror("fork failed");
-		return 1;
-}
-	if (pid == 0) {
-	close(pipefd[1]);
+    if (pipe(pipefd) == -1) {
 
-	int read_byte = read(pipefd[0], buffer, sizeof(buffer) - 1);
-	if (read_byte < 0) {
-	perror("Read failed");
-	return 1;
-}
+        perror("pipe failed");
 
-buffer[read_byte] = '\0';
+        return 1;
 
-printf("Child received : %s/n", buffer);
+    }
 
-close(pipefd[0]);
-} else {
-close(pipefd[0]);
 
-	if (write(pipefd[1], message, strlen(message)) == -1) {
-	perror("write failed");
-	return 1;
-}
+    pid = fork();
+    if (pid == -1) {
+        perror("fork failed");
+        return 1;
 
-close(pipefd[1]);
+    }
 
-wait (NULL);
 
-}
+    if (pid == 0) {
 
+        close(pipefd[1]);
+
+        int read_byte = read(pipefd[0], buffer, sizeof(buffer) - 1);
+        if (read_byte < 0) {
+            perror("Read failed");
+           return 1;
+
+        }
+
+
+
+        buffer[read_byte] = '\0'; 
+        printf("Child received : %s\n", buffer);
+        close(pipefd[0]); 
 return 0;
-}
+    } else {
+        
+        close(pipefd[0]); 
+
+        if (write(pipefd[1], message, strlen(message)) == -1) {
+            perror("write failed");
+            return 1;
+        }
+
+        close(pipefd[1]); 
+        wait(NULL);       
+  }
+
+    return 0;
 }
 
